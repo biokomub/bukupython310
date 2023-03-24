2594ae27259d80f017d971ee374f4dd61bf263ca
Laboratorium Biologi Komputasi dan Bioinformatika Jurusan Biologi FMIPA UB <94002828+biokomub@users.noreply.github.com>
2023-03-24 21:29:40 +0700

    Add files via upload

---
+++ Bab 04/kode4-06.py
@@ -0,0 +1,18 @@
+#contoh match... case...
+
+Nucleobase = "A"
+match Nucleobase:
+    case "A":
+       print("Adenine")
+    case "T":
+       print("Thymine")
+    case "C":
+        print("Cytosine")
+    case "G":
+        print("Guanine")
+    case "U":
+        print("Uracil")
+    case _:
+        print("Bukan nucleobase")
+
+print("Ini diluar match... case...")
---
+++ Bab 04/kode4-08.py
@@ -0,0 +1,16 @@
+#contoh match... case...
+
+Nucleobase = "Z"
+match Nucleobase:
+    case "A":
+       print("Adenine")
+    case "T":
+       print("Thymine")
+    case "C":
+        print("Cytosine")
+    case "G":
+        print("Guanine")
+    case "U":
+        print("Uracil")
+
+print("Ini diluar match... case...")
