bf5cce13310297db771e850eb203b064cac51b4c
Laboratorium Biologi Komputasi dan Bioinformatika Jurusan Biologi FMIPA UB <94002828+biokomub@users.noreply.github.com>
2023-04-01 12:00:28 +0700

    Add files via upload

---
+++ Bab 10/kode10-63.py
@@ -0,0 +1,4 @@
+def Penjumlahan (angka1, angka2):
+	return angka1 + angka2
+	
+print(Penjumlahan(3, 3))
---
+++ Bab 10/kode10-65.py
@@ -0,0 +1,4 @@
+def Penjumlahan (angka1: int, angka2: int) -> int:
+    return angka1 + angka2
+
+print(Penjumlahan("aaa", "bbb"))
